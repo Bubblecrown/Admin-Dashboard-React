21ca18522429553ad238366b826eb7dcb9bf323b
Bubblecrown <suchanya.namwong1@gmail.com>
2022-07-27 23:56:49 +0700

    add: sample form

--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -7,7 +7,18 @@ type LoginPageProps = {
 
 const LoginPage: React.FC<any> = () => {
   const navGate = useNavigate();
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      LoginPage
+      <button
+        onClick={() => {
+          navGate('/register');
+        }}
+      >
+        Register
+      </button>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/components/pages/RegisterPage/RegisterPage.tsx
+++ src/components/pages/RegisterPage/RegisterPage.tsx
@@ -1,3 +1,4 @@
+import { FormLabel } from "@mui/material";
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -7,7 +8,35 @@ type RegisterPageProps = {
 
 const RegisterPage: React.FC<any> = () => {
   const navGate = useNavigate();
-  return <div>RegisterPage</div>;
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          alert("Submitted");
+        }}
+      >
+        <label>First name:</label>
+        <br />
+        <input type="text" id="fname" name="fname" value="John" />
+        <br />
+        <label>Last name:</label>
+        <br />
+        <input type="text" id="lname" name="lname" value="Doe" />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+      <button
+        onClick={() => {
+          // กลับไปหน้าก่อนหน้า
+          navGate(-1);
+        }}
+      >
+        Back to login
+      </button>
+    </div>
+  );
 };
 
 export default RegisterPage;
