01bcc36a6ca0b94ab135e82cf4eb7f1516e7f106
Bubblecrown <suchanya.namwong1@gmail.com>
2022-08-02 22:41:58 +0700

    add: productPage table custom cell image

--- src/components/pages/ProductPage/ProductPage.tsx
+++ src/components/pages/ProductPage/ProductPage.tsx
@@ -1,11 +1,46 @@
 import * as React from "react";
+import { DataGrid, GridColDef, GridRenderCellParams, GridValueGetterParams } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { imageUrl } from "../../../Constants";
 
-type ProductPageProps = {
-  //
-};
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "image", headerName: "IMAGE", width: 70, renderCell:(p:GridRenderCellParams<string>)=>{
+    return(
+    <img
+      src={`${imageUrl}/images/${p.value}?dummy=${Math.random()}`}
+      style={{ width: 70, height: 70, borderRadius: "5%" }}
+    />)
+  }},
+  { field: "name", headerName: "NAME", width: 400 },
+  { field: "price", headerName: "PRICE", width: 130 },
+  { field: "stock", headerName: "AMOUNT", width: 130 },
+];
 
-const ProductPage: React.FC<any> = () => {
-  return <div>ProductPage</div>;
-};
+// const rows = [
+//   { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+//   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+//   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+//   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+//   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+//   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+//   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+//   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+//   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+// ];
 
-export default ProductPage;
+export default function DataTable() {
+  const [rows, setRows] = useState([]);
+  useEffect(() => {
+    const result = axios.get("http://localhost:8085/api/v2/stock/product").then((result) => {
+      setRows(result.data);
+    });
+  }, []);
+
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[5]} checkboxSelection />
+    </div>
+  );
+}
